8f441fec3c5175fdf013d9025e40ec0232c3edab
aror0109@algonquinlive.com <aror0109@algonquinlive.com>
2024-02-08 16:30:55 -0500

    Finished step1. Add float to result.

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -44,12 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
         TextView resultView = findViewById(R.id.resultId);
 
+
         if (!inputAmount.isEmpty()) {
             Float inputAmountDecimal = Float.valueOf(inputAmount);
 
             Float resultFloat = inputAmountDecimal * CONVERSION_RATE;
-
-            resultView.setText( resultFloat + " Euros" );
+            String resultString = getString(R.string.result_euros, resultFloat);
+            resultView.setText( resultString);
         }
     }
 }
