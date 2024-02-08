87ad0d9c63bfe9905327204cefe2a721dc598c56
Ayush Kapoor <kapo0040@algonquinlive.com>
2024-02-08 16:55:07 -0500

    step 4Completed by Ayush

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -1,57 +1,32 @@
 package com.college.converter;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import com.college.converter.databinding.ActivityMainBinding;
-/*
-    TODOs:
-    In groups of 4, complete the following tasks, 1 for each team member:
-    1. Extract all the strings into the strings.xml file and use them in the layout and the activity
-    2. Change the theme of the app to a NoActionBar theme and modify the primary colors
-    3. Add Log messages at the entry/exit of onCreate() and convertCurrency methods. Level should be Info
-    4. Add ViewBinding to the project
-
-    ** Each task must be done in a separate branch and merged to the main branch
-    after completion using a Pull Request.
-    ** Each task must be done by a different team member.
-
-*/
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding variableBinding;
     static private final Float CONVERSION_RATE = 0.80F;
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater()
+        setContentView(binding.getRoot());
 
-        Button buttonConvert = variableBinding.convertButton;
-
-        buttonConvert.setOnClickListener( view ->  {
-            convertCurrency(view);
-        } );
+        binding.convertButton.setOnClickListener(view -> convertCurrency(view)); // Set click listener using binding object
     }
 
     public void convertCurrency(View view) {
-
-        EditText inputView = variableBinding.entryId;
-
-        String inputAmount = inputView.getText().toString();
-
-        TextView resultView = variableBinding.resultId;
+        String inputAmount = binding.entryId.getText().toString(); // Access views using binding object
 
         if (!inputAmount.isEmpty()) {
             Float inputAmountDecimal = Float.valueOf(inputAmount);
 
             Float resultFloat = inputAmountDecimal * CONVERSION_RATE;
 
-            resultView.setText( resultFloat + " Euros" );
+            binding.resultId.setText(resultFloat + " Euros");
         }
     }
 }
